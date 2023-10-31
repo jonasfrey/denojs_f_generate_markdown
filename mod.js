f66fc7e9efafe5ffa3065654878c44133a07408a
jonas.frey@unibe.ch <jonas.frey@unibe.ch>
2023-10-31 17:18:17 +0100

    asdf

--- mod.js
+++ mod.js
@@ -1,6 +1,2 @@
-import { f_generate_markdown } from "./f_generate_markdown.module.js";
+export * from "./f_generate_markdown.module.js";
 
-export {
-    f_generate_markdown,
-    f_generate_markdown__from_s
-}
